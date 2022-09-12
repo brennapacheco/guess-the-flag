b6dedc279848408bdbc98ec6616f1d43d084217e
Brenna <brennapacheco@hotmail.com>
2022-09-12 14:02:04 -0300

    Added buttons and ask question method

--- Guess the Flag/Guess the Flag/Views/ViewController.swift
+++ Guess the Flag/Guess the Flag/Views/ViewController.swift
@@ -8,15 +8,78 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var button1: UIButton!
-    @IBOutlet weak var button2: UIButton!
-    @IBOutlet weak var button3: UIButton!
+
+    private lazy var button1: UIButton = {
+        let element = UIButton()
+        element.translatesAutoresizingMaskIntoConstraints = false
+        element.imageView?.contentMode = .scaleAspectFit
+        element.accessibilityIdentifier = "button1"
+        element.imageView?.layer.borderWidth = 1
+        element.imageView?.layer.borderColor = UIColor.lightGray.cgColor
+        return element
+   }()
+
+    private lazy var button2: UIButton = {
+        let element = UIButton()
+        element.translatesAutoresizingMaskIntoConstraints = false
+        element.imageView?.contentMode = .scaleAspectFit
+        element.accessibilityIdentifier = "button2"
+        element.imageView?.layer.borderWidth = 1
+        element.imageView?.layer.borderColor = UIColor.lightGray.cgColor
+        return element
+   }()
+    
+    private lazy var button3: UIButton = {
+        let element = UIButton()
+        element.translatesAutoresizingMaskIntoConstraints = false
+        element.imageView?.contentMode = .scaleAspectFit
+        element.accessibilityIdentifier = "button3"
+        element.imageView?.layer.borderWidth = 1
+        element.imageView?.layer.borderColor = UIColor.lightGray.cgColor
+        return element
+    }()
+    
+    var countries = [String]()
+    var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        setupButtons()
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
-
+    func setupButtons() {
+        view.addSubview(button1)
+        view.addSubview(button2)
+        view.addSubview(button3)
+        
+        NSLayoutConstraint.activate([
+            button1.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 16),
+            button1.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            button1.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            button1.heightAnchor.constraint(equalToConstant: 100),
+            button1.widthAnchor.constraint(equalToConstant: 200),
+            
+            button2.topAnchor.constraint(equalTo: button1.bottomAnchor, constant: 32),
+            button2.leadingAnchor.constraint(equalTo: button1.leadingAnchor),
+            button2.trailingAnchor.constraint(equalTo: button1.trailingAnchor),
+            button2.heightAnchor.constraint(equalToConstant: 100),
+            button2.widthAnchor.constraint(equalToConstant: 200),
+            
+            button3.topAnchor.constraint(equalTo: button2.bottomAnchor, constant: 32),
+            button3.leadingAnchor.constraint(equalTo: button1.leadingAnchor),
+            button3.trailingAnchor.constraint(equalTo: button1.trailingAnchor),
+            button3.heightAnchor.constraint(equalToConstant: 100),
+            button3.widthAnchor.constraint(equalToConstant: 200),
+        ])
+    }
 }
-
