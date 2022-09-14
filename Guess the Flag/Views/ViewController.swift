1924b7bfb1709efb9235c2963bfd4185c81856f8
Brenna <brennapacheco@hotmail.com>
2022-09-14 15:13:35 -0300

    Add alert controller after 10 questions answered

--- Guess the Flag/Views/ViewController.swift
+++ Guess the Flag/Views/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftConfettiView
 
 class ViewController: UIViewController {
 
@@ -44,10 +45,27 @@ class ViewController: UIViewController {
         return element
     }()
     
+    private lazy var finalButton: UIButton = {
+            let element = UIButton()
+            element.translatesAutoresizingMaskIntoConstraints = false
+            element.imageView?.contentMode = .scaleAspectFit
+            element.accessibilityIdentifier = "finalButton"
+            element.layer.masksToBounds = true
+            element.backgroundColor = UIColor(named: "finalButton")
+            element.tintColor = UIColor.white
+            element.setTitle("Play again?", for: .normal)
+            element.layer.cornerRadius = 20
+            element.addTarget(self, action: #selector(finalAction), for: .touchUpInside)
+            element.isHidden = true
+            return element
+       }()
+    
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
     var scoreTitle = " | Score: "
+    var questionsAsked = 0
+    var confettiView = SwiftConfettiView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -71,28 +89,85 @@ class ViewController: UIViewController {
     
     @objc func buttonTapped(_ sender: UIButton) {
         var title: String
-        
-        if sender.tag == correctAnswer {
-            title = "That's correct!"
-            score += 1
+        questionsAsked += 1
+
+        if questionsAsked == 10 {
+            title = "You answered 10 questions"
+            
+            let dialogMessage = UIAlertController(title: title, message: "Your final score is \(score)", preferredStyle: .alert)
+            
+            let finishGame = UIAlertAction(title: "Quit", style: .default, handler: didFinishGame)
+            
+            dialogMessage.addAction(finishGame)
+            
+            present(dialogMessage, animated: true, completion: nil)
+            
+            showConfetti()
+            
         } else {
-            title = "Ops! That's wrong."
-            score -= 1
+            
+            if sender.tag == correctAnswer {
+                title = "That's correct!"
+                score += 1
+            } else {
+                title = "Ops! That's wrong."
+                score -= 1
+            }
+            
+            let dialogMessage = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            
+            let ok = UIAlertAction(title: "Continue", style: .default, handler: askQuestion)
+            
+            dialogMessage.addAction(ok)
+            
+            present(dialogMessage, animated: true, completion: nil)
         }
-        
-        let dialogMessage = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
-        
-        let ok = UIAlertAction(title: "Continue", style: .default, handler: askQuestion)
-        
-        dialogMessage.addAction(ok)
-        
-        present(dialogMessage, animated: true, completion: nil)
+    }
+    
+    func didFinishGame(_ sender: UIAlertAction! = nil) {
+        removeConfetti()
+        removeButtons()
+        showFinalScreen()
+    }
+    
+    @objc func finalAction() {
+        finalButton.isHidden = true
+        navigationController?.navigationBar.isHidden = false
+        score = 0
+        questionsAsked = 0
+        viewDidLoad()
+    }
+    
+    func removeButtons() {
+        button1.removeFromSuperview()
+        button2.removeFromSuperview()
+        button3.removeFromSuperview()
     }
 
+    func showFinalScreen() {
+        finalButton.isHidden = false
+        navigationController?.navigationBar.isHidden = true
+    }
+
+    func showConfetti() {
+        self.confettiView = SwiftConfettiView(frame: self.view.bounds)
+        self.view.addSubview(confettiView)
+
+        confettiView.type = .confetti
+        confettiView.colors = [UIColor.purple, UIColor.orange, UIColor.black]
+        confettiView.startConfetti()
+    }
+
+    func removeConfetti() {
+        self.confettiView.stopConfetti()
+        self.confettiView.removeFromSuperview()
+    }
+    
     func setupButtons() {
         view.addSubview(button1)
         view.addSubview(button2)
         view.addSubview(button3)
+        view.addSubview(finalButton)
         
         NSLayoutConstraint.activate([
             button1.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 16),
@@ -112,6 +187,12 @@ class ViewController: UIViewController {
             button3.trailingAnchor.constraint(equalTo: button1.trailingAnchor),
             button3.heightAnchor.constraint(equalToConstant: 100),
             button3.widthAnchor.constraint(equalToConstant: 200),
+            
+            finalButton.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 228),
+            finalButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 72),
+            finalButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -72),
+            finalButton.heightAnchor.constraint(equalToConstant: 36),
+            finalButton.widthAnchor.constraint(equalToConstant: 72),
         ])
     }
 }
